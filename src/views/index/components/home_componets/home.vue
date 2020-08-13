75718c585706426a915b33528decdcf34dd55c86
zengxiaolou <44358506+zengxiaolou@users.noreply.github.com>
2020-08-13 23:49:28 +0800

    完成首页样式

--- src/views/index/components/home_componets/home.vue
+++ src/views/index/components/home_componets/home.vue
@@ -10,18 +10,22 @@ INTRODUCTION    文件简介
     <ul class="infinite-list"   v-infinite-scroll="load" >
         <li v-for="i in count" class="infinite-list-item">
             <div class="content">
-                <el-image :src="url" fit="contain"></el-image>
+                <div class="content-header">
+                    <el-image :src="url" fit="contain"></el-image>
+                    <router-link to="/detail">
+                        <h1 class="content-title">这是一个标题</h1>
+                    </router-link>
+                </div>
                 <el-row  :gutter="20" class="introduction">
-                    <el-col :span="4" class="public-date">2020年08月12日</el-col>
-                    <el-col :span="2" class="word-count">2k字</el-col>
-                    <el-col :span="3" class="read-time">大约2分钟</el-col>
-                    <el-col :span="3">阅读数</el-col>
-                    <el-col :span="3">评论数</el-col>
-                    <el-col :span="3">点赞数</el-col>
+                    <el-col :span="4" class="introduction-col public-date"><i class="icon iconfont icon-date"></i>2020年08月12日</el-col>
+                    <el-col :span="2" class="introduction-col word-count"><i class="icon iconfont icon-str"></i>2k字</el-col>
+                    <el-col :span="3" class="introduction-col read-time"><i class="icon iconfont icon-time"></i>大约2分钟</el-col>
+                    <el-col :span="2" class="introduction-col read-num"><i class="icon iconfont icon-view"></i>01</el-col>
+                    <el-col :span="2" class="introduction-col read-comment"><i class="icon iconfont icon-comment"></i>00</el-col>
+                    <el-col :span="2" class="introduction-col read-like"><i class="icon iconfont icon-like"></i>00</el-col>
                 </el-row>
                 <p class="summary">心中的抑郁就像只黑狗，一有机会就咬住我不放。——丘吉尔</p>
             </div>
-
         </li>
     </ul>
 </template>
@@ -57,36 +61,80 @@ INTRODUCTION    文件简介
                 border-radius: 25px;
                 padding-bottom: 10px;
 
-                /deep/ .el-image {
-                    width: 100%;
-                    display: block;
-                    border-radius: 25px;
-                    margin: 0 auto;
+                .content-header {
+                    position: relative;
+                    /deep/ .el-image {
+                        width: 100%;
+                        display: block;
+                        border-radius: 25px;
+                        margin: 0 auto;
+                    }
+                    .content-title{
+                        position: absolute;
+                        bottom: 10px;
+                        left: 20px;
+                        z-index: 1;
+                        color: #ffffff;
+                        font-weight: 900;
+                        font-size: 2.5em;
+                        width: 100%;
+                    }
                 }
+
                 .introduction{
                     margin-top: 10px;
                     margin-bottom: 10px;
-                    .public-date {
+                    font-size: 14px;
+                    font-weight: 500;
+                    i {
+                        padding: 0 5px;
+                    }
+                    .introduction-col {
+                        padding: 5px 0;
                         border-radius: 25px;
-                        background-color: rgba(254, 228, 231, 0.8 );
                         text-align: center;
-                        color: #FA4A63;
                         margin-right: 10px;
+                    }
+                    .public-date {
+                        margin-left: 20px;
+                        background-color: rgba(254, 228, 231, 0.8 );
+                        color: #FA4A63;
+                        i {
+                            font-size: 12px;
+                        }
                     }
                     .word-count {
-                        border-radius: 25px;
                         background-color: rgba(254, 233, 233, 0.8 );
-                        text-align: center;
                         color: #FB8147;
-                        margin-right: 10px;
+                        i {
+                            font-size: 15px;
+                            font-weight: 600;
+                        }
                     }
                     .read-time {
-                        border-radius: 25px;
                         background-color: rgba(255, 243, 222, 1 );
-                        text-align: center;
                         color: #F9B029;
-                        margin-right: 10px;
+                        i {
+                            font-size: 15px;
+                            font-weight: 600;
+                        }
                     }
+                    .read-num {
+                        background-color: rgba(51,213,122,.15);
+                        color: #33D57A;
+                    }
+                    .read-comment {
+                        background-color: rgba(26,152,255,.15);
+                        color: #1a98ff;
+
+                    }
+                    .read-like {
+                        background-color: rgba(255,78,106,.15);
+                        color: #ff4e6a;
+                    }
+                }
+                .summary {
+                    margin-left: 20px;
                 }
             }
         }
