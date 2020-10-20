cdb914b06b4c89badc40925b8db643e83eecce8f
zengxiaolou <44358506+zengxiaolou@users.noreply.github.com>
2020-10-20 15:40:56 +0800

    完成TuiEditor图片上传七牛云

--- src/components/MarkdownEditor/markdown.vue
+++ src/components/MarkdownEditor/markdown.vue
@@ -14,6 +14,9 @@ INTRODUCTION    文件简介
 import 'codemirror/lib/codemirror.css'; // Editor's Dependency Style
 import '@toast-ui/editor/dist/toastui-editor.css'; // Editor's Style
 import Editor from '@toast-ui/editor';
+import {getQiNiuToken} from "api/utils";
+import md5 from "js-md5";
+import baseSetting from "store/baseSetting";
 export default {
     name: "index",
     props: ['markdown'],
@@ -35,10 +38,40 @@ export default {
                 previewStyle: 'vertical',
                 initialValue: "这里书写你的评论",
             });
+            // 删除默认监听事件
+            this.editor.eventManager.removeEventHandler('addImageBlobHook')
+            // 添加自定义监听事件
+            this.editor.eventManager.listen('addImageBlobHook', (blob, callback) => {
+                // 此处填写自己的上传逻辑，url为上传后的图片地址
+                this.upload(blob, url => {
+                    callback(url)
+                })
+            })
         },
         changeValue(){
             this.$emit('changeMarkdown', this.editor.getMarkdown())
         },
+        upload(file, callback) {
+            const formData = new FormData()
+            formData.append('file', file)
+            let timestamp = new Date().getTime();
+            let new_name = md5(timestamp + file.name);
+            new_name = new_name.substring(0,16)
+            getQiNiuToken({"name": new_name}).then(res => {
+                formData.append('token', res['token'])
+                formData.append('key', new_name)
+                const ajax = new XMLHttpRequest()
+                ajax.open('POST', 'http://upload-z2.qiniup.com', true)
+                ajax.send(formData)
+                ajax.onreadystatechange = function() {
+                    if (ajax.readyState === 4) {
+                        if ((ajax.status >= 200 && ajax.status < 300) || ajax.status === 304) {
+                            callback(baseSetting.QiNiuHost + new_name)
+                        }
+                    }
+                }
+            });
+        }
     },
     mounted() {
         this.initialize()
